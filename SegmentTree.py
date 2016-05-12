26a29a7a4bda8ca063cc7b6c012bf753820f802b
thinksource <foretribe@gmail.com>
2016-05-12 20:41:09 +1000

    add SegmentTree.py

---
+++ SegmentTree.py
@@ -0,0 +1,15 @@
+import math
+class SegmentTreeNode:
+    def __init__(self, start, end):
+        self.start, self.end=start, end
+        self.left,self.right=None, None
+
+class SegmentTree:
+
+    def build(self,start, end):
+        node=new SegmentTreeNode(start, end)
+        if(start!=end):
+            firstend=math.floor((start + end) / 2)
+            node.left=self.build(start,firstend)
+            node.right=self.build(firstend+1, end)
+        return node
