d466e29a14c16537d89379f300d5ef2adada5135
Sheng Lu <Sheng Lu>
2016-05-23 10:11:54 +0800

    added triangle-count

---
+++ triangle-count.py
@@ -0,0 +1,14 @@
+class Solution:
+    def triangleCount(self, S):
+        edges = sorted(S)
+        ans=0
+        for i in range(len(edges)):
+            left=0
+            right=i-1;
+            while(left<right):
+                if(edges[left]+edges[right]>edges[i]):
+                    ans+=right-left
+                    right-=1
+                else:
+                    left++
+        return ans
