3f6c66c487eac25e4e67d9892c43a5594e7b0809
thinksource <foretribe@gmail.com>
2016-06-02 23:11:24 +0800

    change to correct version

--- longestConsecutiveSequence.py
+++ longestConsecutiveSequence.py
@@ -6,28 +6,21 @@ class Solution:
     """
     def longestConsecutive(self, num):
         # write your code here
-    	value_count={}
-    	for i in num:
-    		t=i
-    		if (i+1 in value_count or i-1 in value_count) and (not i in value_count):
-    			m=i
-    			if (i+1 in value_count and i-1 in value_count):
-    				value_count[i-1] += value_count[i+1]
-    				value_count[i+1] = value_count[i-1]
-    			while t + 1 in value_count:
-    				value_count[t + 1] += 1
-    				if t in value_count:
-    					value_count[t + 1]=value_count[t]
-    				t += 1
-    			while m - 1 in value_count:
-    				value_count[m - 1] += 1
-    				if m in value_count:
-    					value_count[m - 1]=value_count[m]
-    				m -= 1
-    			if i+1 in value_count:
-    				value_count[i]=value_count[i + 1]
-    			if i-1 in value_count:
-    				value_count[i]=value_count[i - 1]
-    		if not i in value_count:
-    			value_count[i] = 1
-        return value_count[max(value_count, key=value_count.get)]
+    	nums_set=set()
+        rmax=0
+        for i in num:
+            nums_set.add(i)
+        for j in num:
+            left=j-1
+            right=j+1
+            count=1
+            while left in nums_set:
+                count+=1
+                nums_set.remove(left)
+                left-=1
+            while right in nums_set:
+                count+=1
+                nums_set.remove(right)
+                right+=1
+            rmax=max(rmax, count)
+        return rmax
