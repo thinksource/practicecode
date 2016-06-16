bed69887a584a5dfa2f069719ed2052179e224e8
thinksource <foretribe@gmail.com>
2016-06-16 17:59:53 +0800

    add BinaryTree.java

---
+++ java/BinaryTree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class BinaryTree {
+    private void dfs(HashMap<Integer, Integer> depthToValue, TreeNode node, int depth) {
+        if (node == null) {
+            return;
+        }
+
+        depthToValue.put(depth, node.val);
+        dfs(depthToValue, node.left, depth + 1);
+        dfs(depthToValue, node.right, depth + 1);
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        HashMap<Integer, Integer> depthToValue = new HashMap<Integer, Integer>();
+        dfs(depthToValue, root, 1);
+
+        int depth = 1;
+        List<Integer> result = new ArrayList<Integer>();
+        while (depthToValue.containsKey(depth)) {
+            result.add(depthToValue.get(depth));
+            depth++;
+        }
+        return result;
+    }
+}
